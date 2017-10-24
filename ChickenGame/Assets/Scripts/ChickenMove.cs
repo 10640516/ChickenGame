7d8e67f879df2d956c5cf668c642122e9d0610fa
Unknown <mormondragon@yahoo.com>
2017-10-23 20:10:56 -0600

    Created chicken and fox/wolf movement

---
+++ ChickenGame/Assets/Scripts/ChickenMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenMove : MonoBehaviour {
+
+	public Rigidbody enemy;
+	public float moveSpeed;
+	public Transform target;
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered chickens trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*-moveSpeed*Time.deltaTime);
+			
+		}
+
+	}
+}
---
+++ ChickenGame/Assets/Scripts/VixAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VixAI : MonoBehaviour {
+
+	public Rigidbody enemy;
+	public float moveSpeed;
+	public Transform target;
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered vixen wolf trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+			
+		}
+
+	}
+}
